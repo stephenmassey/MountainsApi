a0e4416fe7de250123568f6a2229f33a143b1ce4
stephenmassey <stevmassey@gmail.com>
2017-01-18 10:17:21 -0700

    Suppress properties with null value

--- src/Mountains.V1.Web/Global.asax.cs
+++ src/Mountains.V1.Web/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using Mountains.V1.Web.Infrastructure;
+using Newtonsoft.Json;
 using StructureMap;
 using System.Web;
 using System.Web.Http;
@@ -12,6 +13,10 @@ namespace Mountains.V1.Web
         {
             _container = TypeRegistry.RegisterTypes();
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+            };
 
             GlobalConfiguration.Configure(RouteRegistry.RegisterRoutes);
 
